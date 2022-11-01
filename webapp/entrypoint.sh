2dd0902d86ff9118727aa8b99be21518ed9be4b2
Mesake <mesake.navunawa@gmail.com>
2022-11-01 23:16:20 +1200

    changed database name

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh                                                                       
 
+# remove all tables
+python manage.py flush --no-input
+
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
