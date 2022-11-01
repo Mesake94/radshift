e08e93b9788f2eed515c7a59fa49e32ed3c0a3f0
Mesake <mesake.navunawa@gmail.com>
2022-11-02 00:24:34 +1200

    updated django settings allowed hosts

--- webapp/project/settings.py
+++ webapp/project/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = env('SECRET_KEY', default='q3i6z@j6arvr&fswvw=#t315$&^@9pf5gi+=-3eg
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
